524ace5db496cfb2975a774457dbd26c2c42e3b9
pilots2013 <56370765+pilots2013@users.noreply.github.com>
2019-10-23 22:34:48 +0200

    Added comments and some renaming
    Added some comments to explaing the code and renamed some variable names to make their purpose more clear

--- source/ProjectEulerSolutions.cpp
+++ source/ProjectEulerSolutions.cpp
@@ -1,9 +1,9 @@
 //============================================================================
-// Name        : 3.cpp
+// Name        : ProjectEulerSolutions.cpp
 // Author      : Pilots2013
-// Version     :
+// Version     : 0.0.1
 // Copyright   : 
-// Description : Hello World in C++, Ansi-style
+// Description : Solutions to ProjectEuler.net problems
 //============================================================================
 
 #include <iostream>
@@ -14,7 +14,7 @@
 
 constexpr int32_t MAX_NUMBER_VALUE = 1000;
 
-int sum_first_3and5muls(int floor_value)
+int sum_first_3and5muls(int floor_value) //Sums multiples of 3 and 5 up to floor_value
 {
 	int number_sum = 0;
 	for(int i = 1; i < floor_value; ++i)
@@ -26,7 +26,7 @@ int sum_first_3and5muls(int floor_value)
 	}
 	return number_sum;
 }
-int fib_even_sum(int floor_value)
+int fib_even_sum(int floor_value) //Sums the even fibonacci numbers up to floor_value
 {
 	int number_sum = 0;
 	int current_fib = 1; //Current fibonacci number
@@ -39,7 +39,7 @@ int fib_even_sum(int floor_value)
 		current_fib += prev_fib;
 		prev_fib = temp_fib;
 
-        if(current_fib % 2 == 0)
+        if(current_fib % 2 == 0) //Checks if current_fib is an even number
 		{
 			number_sum += current_fib;
 		}
@@ -49,7 +49,9 @@ int fib_even_sum(int floor_value)
 }
 
 
-int largest_prime(uint64_t n) //Precondition: n is larger or equal than 2
+int largest_prime(uint64_t n) //Precondition: n is larger or equal than 2.
+							  //uint64 is necessary for large inputs
+							  //This needs optimization
 {
 	int largest_prime {0};
 	bool is_prime {0};
@@ -85,34 +87,38 @@ int largest_prime(uint64_t n) //Precondition: n is larger or equal than 2
 	return largest_prime;
 }
 
-int largest_palindrome(const int product_digits)
+int largest_palindrome(const int product_digits) //Returns the highest integer palindrome obtained by the product of integers
+												 //of product_digits number of digits
 {
 
-	int i = round(pow(10, product_digits - 1));
-	int j = i;
+	int first_term = round(pow(10, product_digits - 1)); // This will make both first_term and second_term start from the minimum value of
+														 // product_digits number of digits
+	int second_term = first_term;
 	int product {0};
 	int palindrome = 1;
 
 	std::string check1, check2;
-	const int max_value = round(pow(10, product_digits));
+	const int max_value = round(pow(10, product_digits)); //Round functions is used due to some miscalculations where max_valued was truncated to 99
 
-	while (i < max_value)
+	while (first_term < max_value)
 	{
 
-		j = i;
-		while(j < max_value)
+		second_term = first_term;
+		while(second_term < max_value)
 		{
-			product = i * j;
-			++j;
-			check1 = std::to_string(product);
-			check2 = check1;
-			std::reverse(check2.begin(), check2.end());
-			if(check1 == check2 && product > palindrome)
+			product = first_term * second_term;
+			++second_term;
+
+			check1 = std::to_string(product);  //These instructions convert the product to string
+			check2 = check1;				   //then checks if the product string and it's reverse are equal
+			std::reverse(check2.begin(), check2.end()); 	//REMINDER: This could be a function of it's own
+
+			if(check1 == check2 && product > palindrome) //Checks for equality and if the candidate palindrome is largest than palindrome
 			{
 				palindrome = std::stoi(check1);
 			}
 		}
-		++i;
+		++first_term;
 
 
 	}
