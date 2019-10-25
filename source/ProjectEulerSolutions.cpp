01aa799c4646ebcd12d4f4ba48dc8b0308ae7c67
pilots2013 <56370765+pilots2013@users.noreply.github.com>
2019-10-25 20:58:04 +0200

    Refactoring and type changes

--- source/ProjectEulerSolutions.cpp
+++ source/ProjectEulerSolutions.cpp
@@ -14,20 +14,23 @@
 #include "palindrome.hpp"
 #include "numerics.hpp"
 
-
-
-int fib_even_sum(int floor_value) //Sums the even fibonacci numbers up to floor_value
+inline void fib_advance(uint64_t& previous, uint64_t& current)
 {
-	int number_sum = 0;
-	int current_fib = 1; //Current fibonacci number
-	int prev_fib = 0; //Previous fibonacci number
-	int temp_fib = 0; //Temp fibonacci number used in the loop
+	uint64_t temporary {current}; //Temporary used as support to advance in the fib series
+	current += previous;
+	previous = temporary;
+}
+
+uint64_t fib_even_sum(uint64_t floor_value) //Sums the even fibonacci numbers up to floor_value
+{
+	uint64_t number_sum {0};
+	uint64_t current_fib {1}; //Current fibonacci number
+	uint64_t prev_fib {0}; //Previous fibonacci number
+
 
 	while(current_fib < floor_value)
 	{
-		temp_fib = current_fib;
-		current_fib += prev_fib;
-		prev_fib = temp_fib;
+		fib_advance(prev_fib, current_fib);
 
         if(current_fib % 2 == 0) //Checks if current_fib is an even number
 		{
@@ -35,6 +38,7 @@ int fib_even_sum(int floor_value) //Sums the even fibonacci numbers up to floor_
 		}
 
 	}
+
 	return number_sum;
 }
 
